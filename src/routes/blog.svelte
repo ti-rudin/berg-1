e206d78791e3e96f356882874f02ee2917361361
Alexander Rudin <ax.rudin@gmail.com>
2022-04-21 23:17:26 +0000

    11

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -31,7 +31,7 @@
 <script>
 	import IndexCard from '../components/IndexCard.svelte';
 
-	export let page;
+	//export let page;
 	export let list;
 
 	// technically this is a slighlty different type because doesnt have 'content' but we'll let it slide
